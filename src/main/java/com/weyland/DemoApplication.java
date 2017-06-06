4354dcc03f3383e6a61e088d45bc7c11abf8d42a
EvilAccountant <344890472@qq.com>
2017-06-06 14:42:37 +0800

    update

--- src/main/java/com/weyland/DemoApplication.java
+++ src/main/java/com/weyland/DemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication {
+//	打包时启用
+//		extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
